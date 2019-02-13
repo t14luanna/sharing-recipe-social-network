0abe36a8d478aaa04476a45e6397ee0f99772d50
t14luanna <t14.luanna@gmail.com>
2019-02-13 12:47:47 +0700

    Setup Project

---
+++ SRSN/SRSN.ClientView/Pages/SubmitRecipe.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SRSN.ClientView.Pages
+{
+    public class SumitRecipeModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
