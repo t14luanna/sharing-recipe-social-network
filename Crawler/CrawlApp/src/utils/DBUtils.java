d60fd58f43b53b0334752743fd541094fff1e8ba
BaoLKSE62460 <baolkse62460@fpt.edu.vn>
2019-02-19 18:28:53 +0700

    ToptenUser, Login, Logout

--- Crawler/CrawlApp/src/utils/DBUtils.java
+++ Crawler/CrawlApp/src/utils/DBUtils.java
@@ -19,7 +19,7 @@ public class DBUtils {
      String sqlInstanceName = "SQLEXPRESS";
      String database = "FoodCrawlData";
      String userName = "sa";
-     String password = "123";
+     String password = "123456789";
  
      return getConnection(hostName, sqlInstanceName, database,
              userName, password);
---
+++ SRSN/SRSN.Client_View/wwwroot/js/getTopTen.js
@@ -0,0 +1,34 @@
+﻿const readTopTenUser = (account) =>
+    `<li>
+                        <div class="single-chef">
+                            <a href="#"><img src="recipepress/images/temp-images/chef-team1.jpg" alt="team" /></a>
+                            <div class="chef-detail">
+                                <div class="chef-detail-inner">
+                                    <h4>${account.FirstName}</h4>
+                                    <span class="type">executive chef</span>
+                                    <p>
+                                        Nam ornare arcu turpis ne congues withCurabitur quis euismod ur.Nam ornare arcu turpis
+                                    </p>
+                                    <ul class="social-icons-chef">
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>`
+
+const callAccountApi = async () => {
+    var response = await fetch("https://localhost:44361/api/account/get-top-ten");
+    var data = await response.json();
+    for (var var item of data) {
+        let element = readPopularUser(item);
+        $("#chef-team").append(element);
+    }
+}
+
+$(document).ready(e) => {
+    callAccountApi();
+}
